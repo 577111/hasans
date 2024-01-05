2fe73217ef57fd7e634a2a495b810be7d2a0bb61
Rakib <sohrabhosen16105@gamil.com>
2024-01-05 18:44:26 +0600

    hi

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`);
+
+
+})
+
+app.get("/", (req,res)=>{
+    res.send({
+        name:"shohrab hasan"
+    })
 })
