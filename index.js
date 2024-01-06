53e47605848a32feb3ecf7c5916f738f900e7386
Rakib <sohrabhosen16105@gamil.com>
2024-01-06 23:28:49 +0600

    add public folder

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express=require('express')
 const app = express();
 const router = require('./routes/users.route');
 
-app.set("view engine","ejs")
+// app.set("view engine","ejs")
 
+app.use(express.static("public"));
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
